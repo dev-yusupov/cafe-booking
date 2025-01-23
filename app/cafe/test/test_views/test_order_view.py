567afaabcbef090b1b588bf52d7c543be0359219
Bobur Yusupov <bobur.yu06@mail.ru>
2025-01-23 22:33:53 +0100

    test order view's code quality improved

--- app/cafe/test/test_views/test_order_view.py
+++ app/cafe/test/test_views/test_order_view.py
@@ -2,46 +2,59 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from cafe.models import Order
 
-
 class OrderViewTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.order = Order.objects.create(
             table_number=1,
-            items=[{"name": "Coffee", "quantity": 2, "price": 3.5}],
-            status=Order.STATUS_PENDING,
+            items=[{'name': 'Coffee', 'quantity': 2, 'price': 3.5}],
+            status=Order.STATUS_PENDING
         )
 
     def test_order_list_view(self):
-        response = self.client.get(reverse("order_list"))
+        """
+        Test the order list view.
+        """
+        response = self.client.get(reverse('order_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "cafe/order.html")
+        self.assertTemplateUsed(response, 'cafe/order.html')
         self.assertContains(response, self.order.id)
 
     def test_order_detail_view(self):
-        response = self.client.get(reverse("order_detail", args=[self.order.id]))
+        """
+        Test the order detail view.
+        """
+        response = self.client.get(reverse('order_detail', args=[self.order.id]))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "cafe/order_detail.html")
+        self.assertTemplateUsed(response, 'cafe/order_detail.html')
         self.assertContains(response, self.order.id)
 
     def test_order_create_view(self):
+        """
+        Test the order create view.
+        """
         data = {
-            "table_number": 2,
-            "items": [{"name": "Tea", "quantity": 1, "price": 2.0}],
+            'table_number': 2,
+            'items': [{'name': 'Tea', 'quantity': 1, 'price': 2.0}]
         }
-        response = self.client.post(
-            reverse("order_create"), data, content_type="application/json"
-        )
+        response = self.client.post(reverse('order_create'), data, content_type='application/json')
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Order.objects.count(), 2)
 
     def test_order_update_status_view(self):
-        data = {"status": Order.STATUS_READY}
-        response = self.client.post(
-            reverse("order_update_status", args=[self.order.id]),
-            data,
-            content_type="application/json",
-        )
+        """
+        Test the order update status view.
+        """
+        data = {'status': Order.STATUS_READY}
+        response = self.client.post(reverse('order_update_status', args=[self.order.id]), data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.order.refresh_from_db()
         self.assertEqual(self.order.status, Order.STATUS_READY)
+
+    def test_order_delete_view(self):
+        """
+        Test the order delete view.
+        """
+        response = self.client.delete(reverse('order_delete', args=[self.order.id]))
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Order.objects.count(), 0)
